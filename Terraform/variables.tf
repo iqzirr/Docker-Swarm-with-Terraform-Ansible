0376357268eaf6e02228002d56649bd0ab86be70
Rizqi Firmansyah <rizqi@mit.id>
2022-03-30 09:59:21 +0700

    Update: Some Updates

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,44 +1,44 @@
 # Proxmox API Credentials
 variable "user_token_id" {
   type = string
-  default = "terraform-prov@pve!JAbAMSfkcCkWyqjmDt3VfegyNee9eeEDFtms66YQpBgZKaSS9B"
+  default = "your user_token_id"
 }
 
 variable "user_token_secret" {
   type = string
-  default = "2f5fe4b2-991c-4932-8926-ecc7f0e2d7d2"
+  default = "your user_token_secret"
 }
 
 variable "proxmox_api_url" {
   type = string
-  default = "https://172.16.20.206:8006/api2/json"
+  default = " your proxmox_api_url, ex= https://123.123.123.123:8006/api2/json"
 }
 
 
 # Proxmox Host Config
 variable "template_name" {
   type = string
-  default = "debian11-cloudinit-template"
+  default = "your vm template_name in Proxmox, ex= debian11-cloudinit-template"
 }
 
 variable "proxmox_host" {
   type = string  
-  default = "mitsvr06"
+  default = "your proxmox_host, ex= svr007"
 }
 
 
 # Proxmox VM Template Credential and Configurations, use privileged user for installing Docker
 variable "proxmox_vm_user"{
   type = string
-  default = "root"
+  default = "your proxmox_vm_user"
 }
 
 variable "proxmox_vm_password"{
   type = string
-  default = "mit"
+  default = "your proxmox_vm_password"
 }
 
 variable "proxmox_vm_ssh_port"{
   type = string
-  default = "22"
+  default = "your proxmox_vm_ssh_port"
 }
